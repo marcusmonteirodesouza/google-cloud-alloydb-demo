41010a2a0db95e6edc4e468eed1d6dc1cd31ad76
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-09-23 15:40:28 -0400

    Create database cluster with a generated initial user name

--- terraform/modules/app/alloydb.tf
+++ terraform/modules/app/alloydb.tf
@@ -1,3 +1,28 @@
+# AlloyDB initial user name
+resource "random_password" "alloydb_initial_user_name" {
+  length  = 16
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "google_secret_manager_secret" "alloydb_initial_user_name" {
+  secret_id = "demo-app-alloydb-initial-user-name"
+
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "alloydb_initial_user_name" {
+  secret      = google_secret_manager_secret.alloydb_initial_user_name.id
+  secret_data = random_password.alloydb_initial_user_name.result
+}
+
 # AlloyDB initial user password
 resource "random_password" "alloydb_initial_user_password" {
   length = 16
@@ -25,10 +50,9 @@ resource "google_alloydb_cluster" "app" {
   cluster_id   = "demo-app-alloydb-cluster"
   location     = var.alloydb_region
   network      = data.google_compute_network.app.id
-  display_name = "Demo App AlloyDB cluster"
 
   initial_user {
-    user     = "postgres"
+    user     = random_password.alloydb_initial_user_name.result
     password = random_password.alloydb_initial_user_password.result
   }
 }
